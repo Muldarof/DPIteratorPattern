55ea648e0fc85558b80b2664547ec62be509e275
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-10-12 22:14:43 -0400

    refactor updated Main with new sample code

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,11 @@ public class Main {
     public static void main(String[] args) {
 
         PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
-        ArrayList<MenuItem> breakfastItems = pancakeHouseMenu.getMenuItems();
-
         DinerMenu dinerMenu = new DinerMenu();
-        MenuItem[] lunchItems = dinerMenu.getMenuItems();
 
-        for(int i = 0; i < breakfastItems.size(); i++){
-            MenuItem menuItem = breakfastItems.get(i);
-            System.out.println(menuItem.getName() + " ");
-            System.out.println(menuItem.getPrice() + " ");
-            System.out.println(menuItem.getDescription() + " ");
-        }
-        for(int i = 0; i < lunchItems.length; i++){
-            MenuItem menuItem = lunchItems[i];
-            System.out.println(menuItem.getName() + " ");
-            System.out.println(menuItem.getPrice() + " ");
-            System.out.println(menuItem.getDescription() + " ");
-        }
+        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+
+        waitress.printMenu();
 
 
     }
