c4f0618937bd5155afa2cb559675b9965bc73a0b
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-10-12 21:17:53 -0400

    added code to main to display menu items

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        ArrayList<MenuItem> breakfastItems = pancakeHouseMenu.getMenuItems();
+
+        DinerMenu dinerMenu = new DinerMenu();
+        MenuItem[] lunchItems = dinerMenu.getMenuItems();
+
+        for(int i = 0; i < breakfastItems.size(); i++){
+            MenuItem menuItem = breakfastItems.get(i);
+            System.out.println(menuItem.getName() + " ");
+            System.out.println(menuItem.getPrice() + " ");
+            System.out.println(menuItem.getDescription() + " ");
+        }
+        for(int i = 0; i < lunchItems.length; i++){
+            MenuItem menuItem = lunchItems[i];
+            System.out.println(menuItem.getName() + " ");
+            System.out.println(menuItem.getPrice() + " ");
+            System.out.println(menuItem.getDescription() + " ");
+        }
+
 
     }
 }
